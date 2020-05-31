9e0e7b722ac343878eeae19add27135d8476af1e
GlaAndry <a31101995A>
2020-05-31 12:01:14 +0200

    ProvaTest

---
+++ moduleTestBookkeeper/src/test/java/unit/FileInfoTest.java
@@ -0,0 +1,14 @@
+package unit;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FileInfoTest {
+
+    @Test
+    public void provaTest(){
+        Assert.assertEquals(0,0);
+    }
+
+}
